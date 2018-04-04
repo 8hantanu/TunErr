206c7cd5c07c09b50f09adb3cf1ac4565c825161
WiCK3D-POiSON <wick3d.poison@gmail.com>
2018-04-04 20:00:14 +0530

    Initial commit Version 0.1

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-######################################################################
-# Might want to change NOTE_MIN and NOTE_MAX especially for guitar/bass.
-# Probably want to keep FRAME_SIZE and FRAMES_PER_FFT to be powers of two.
-
 NOTE_MIN = 60       # C4
 NOTE_MAX = 69       # A4
 FSAMP = 22050       # Sampling frequency in Hz
